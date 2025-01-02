37daa58645f01db7b3d770cd538546f78d7b38bd
Michael Ansolis <ansolis@gmail.com>
2025-01-02 00:33:54 -0800

    Update clang-format settings and format lcd.c according to the new settings.

--- Demo_1/ST7735/lcd.c
+++ Demo_1/ST7735/lcd.c
@@ -5,249 +5,258 @@
 
 #include "main.h"
 
-//SPI display interface
-//LCD_RST
+// SPI display interface
+// LCD_RST
 #define LCD_RST_SET
 #define LCD_RST_RESET
-//LCD_RS/DC
+// LCD_RS/DC
 #define LCD_RS_SET      HAL_GPIO_WritePin(LCD_WR_RS_GPIO_Port,LCD_WR_RS_Pin,GPIO_PIN_SET)//PC4
 #define LCD_RS_RESET    HAL_GPIO_WritePin(LCD_WR_RS_GPIO_Port,LCD_WR_RS_Pin,GPIO_PIN_RESET)
-//LCD_CS
+// LCD_CS
 #define LCD_CS_SET      HAL_GPIO_WritePin(LCD_CS_GPIO_Port,LCD_CS_Pin,GPIO_PIN_SET)
 #define LCD_CS_RESET    HAL_GPIO_WritePin(LCD_CS_GPIO_Port,LCD_CS_Pin,GPIO_PIN_RESET)
-//SPI Driver
+// SPI Driver
 #define SPI spi4
 #define SPI_Drv (&hspi4)
 #define delay_ms HAL_Delay
 #define get_tick HAL_GetTick
-//LCD_Brightness timer
-#define LCD_Brightness_timer &htim1
+// LCD_Brightness timer
+#define LCD_Brightness_timer   &htim1
 #define LCD_Brightness_channel TIM_CHANNEL_2
 
 static int32_t lcd_init(void);
 static int32_t lcd_gettick(void);
-static int32_t lcd_writereg(uint8_t reg,uint8_t* pdata,uint32_t length);
-static int32_t lcd_readreg(uint8_t reg,uint8_t* pdata);
-static int32_t lcd_senddata(uint8_t* pdata,uint32_t length);
-static int32_t lcd_recvdata(uint8_t* pdata,uint32_t length);
+static int32_t lcd_writereg(uint8_t reg, uint8_t* pdata, uint32_t length);
+static int32_t lcd_readreg(uint8_t reg, uint8_t* pdata);
+static int32_t lcd_senddata(uint8_t* pdata, uint32_t length);
+static int32_t lcd_recvdata(uint8_t* pdata, uint32_t length);
 
 ST7735_IO_t st7735_pIO = {
-	lcd_init,
-	NULL,
-	0,
-	lcd_writereg,
-	lcd_readreg,
-	lcd_senddata,
-	lcd_recvdata,
-	lcd_gettick
+    lcd_init,     // Init function
+    NULL,         // DeInit function
+    0,            // Address
+    lcd_writereg, // Write register function
+    lcd_readreg,  // Read register function
+    lcd_senddata, // Send data function
+    lcd_recvdata, // Receive data function
+    lcd_gettick   // Get tick function
 };
 
 ST7735_Object_t st7735_pObj;
-uint32_t st7735_id;
+uint32_t        st7735_id;
 
-void LCD_Test(void)
-{
-	uint8_t text[20];
-
-	#ifdef TFT96
-	ST7735Ctx.Orientation = ST7735_ORIENTATION_LANDSCAPE_ROT180;
-	ST7735Ctx.Panel = HannStar_Panel;
-	ST7735Ctx.Type = ST7735_0_9_inch_screen;
-	#elif TFT18
-	ST7735Ctx.Orientation = ST7735_ORIENTATION_PORTRAIT;
-	ST7735Ctx.Panel = BOE_Panel;
-	ST7735Ctx.Type = ST7735_1_8a_inch_screen;
-	#else
-	error "Unknown Screen"
-
-	#endif
-
-	ST7735_RegisterBusIO(&st7735_pObj,&st7735_pIO);
-	ST7735_LCD_Driver.Init(&st7735_pObj,ST7735_FORMAT_RBG565,&ST7735Ctx);
-	ST7735_LCD_Driver.ReadID(&st7735_pObj,&st7735_id);
-
-	LCD_SetBrightness(0);
+void LCD_Test(void) {
+    uint8_t text[20];
 
 #ifdef TFT96
-	extern unsigned char WeActStudiologo_160_80[];
-	ST7735_LCD_Driver.DrawBitmap(&st7735_pObj,0,0,WeActStudiologo_160_80);
+    ST7735Ctx.Orientation = ST7735_ORIENTATION_LANDSCAPE_ROT180;
+    ST7735Ctx.Panel       = HannStar_Panel;
+    ST7735Ctx.Type        = ST7735_0_9_inch_screen;
 #elif TFT18
-	extern unsigned char WeActStudiologo_128_160[];
-	ST7735_LCD_Driver.DrawBitmap(&st7735_pObj,0,0,WeActStudiologo_128_160);
+    ST7735Ctx.Orientation = ST7735_ORIENTATION_PORTRAIT;
+    ST7735Ctx.Panel       = BOE_Panel;
+    ST7735Ctx.Type        = ST7735_1_8a_inch_screen;
+#else
+    error "Unknown Screen"
+
+#endif
+
+    ST7735_RegisterBusIO(&st7735_pObj, &st7735_pIO);
+    ST7735_LCD_Driver.Init(&st7735_pObj, ST7735_FORMAT_RBG565, &ST7735Ctx);
+    ST7735_LCD_Driver.ReadID(&st7735_pObj, &st7735_id);
+
+    LCD_SetBrightness(0);
+
+#ifdef TFT96
+    extern unsigned char WeActStudiologo_160_80[];
+    ST7735_LCD_Driver.DrawBitmap(&st7735_pObj, 0, 0, WeActStudiologo_160_80);
+#elif TFT18
+    extern unsigned char WeActStudiologo_128_160[];
+    ST7735_LCD_Driver.DrawBitmap(&st7735_pObj, 0, 0, WeActStudiologo_128_160);
 #endif
 
     uint32_t tick = get_tick();
-	while (HAL_GPIO_ReadPin(BUTTON_GPIO_Port, BUTTON_Pin) != GPIO_PIN_SET)
-	{
-		delay_ms(10);
+    while (HAL_GPIO_ReadPin(BUTTON_GPIO_Port, BUTTON_Pin) != GPIO_PIN_SET) {
+        delay_ms(10);
 
-		if (get_tick() - tick <= 1000)
-			LCD_SetBrightness((get_tick() - tick) * 100 / 1000);
-		else if (get_tick() - tick <= 3000)
-		{
-			sprintf((char *)&text, "%03lu", (get_tick() - tick - 1000) / 10);
-			LCD_ShowString(ST7735Ctx.Width - 30, 1, ST7735Ctx.Width, 16, 16, text);
-			ST7735_LCD_Driver.FillRect(&st7735_pObj, 0, ST7735Ctx.Height - 3, (get_tick() - tick - 1000) * ST7735Ctx.Width / 2000, 3, 0xFFFF);
-		}
-		else if (get_tick() - tick > 3000)
-			break;
-	}
-	while (HAL_GPIO_ReadPin(BUTTON_GPIO_Port, BUTTON_Pin) == GPIO_PIN_SET)
-	{
-		delay_ms(10);
-	}
-	LCD_Light(0, 300);
+        if (get_tick() - tick <= 1000)
+            LCD_SetBrightness((get_tick() - tick) * 100 / 1000);
+        else if (get_tick() - tick <= 3000) {
+            sprintf((char*)&text, "%03lu", (get_tick() - tick - 1000) / 10);
+            LCD_ShowString(ST7735Ctx.Width - 30, 1, ST7735Ctx.Width, 16, 16,
+                           text);
+            ST7735_LCD_Driver.FillRect(
+                &st7735_pObj, 0, ST7735Ctx.Height - 3,
+                (get_tick() - tick - 1000) * ST7735Ctx.Width / 2000, 3, 0xFFFF);
+        } else if (get_tick() - tick > 3000) {
+            break;
+        }
+    }
+    while (HAL_GPIO_ReadPin(BUTTON_GPIO_Port, BUTTON_Pin) == GPIO_PIN_SET) {
+        delay_ms(10);
+    }
+    LCD_Light(0, 300);
 
-	ST7735_LCD_Driver.FillRect(&st7735_pObj, 0, 0, ST7735Ctx.Width,ST7735Ctx.Height, BLACK);
+    ST7735_LCD_Driver.FillRect(&st7735_pObj, 0, 0, ST7735Ctx.Width,
+                               ST7735Ctx.Height, BLACK);
 
-	sprintf((char *)&text, "WeAct Studio");
-	LCD_ShowString(4, 4, ST7735Ctx.Width, 16, 16, text);
-	sprintf((char *)&text, "STM32H7xx 0x%lX", HAL_GetDEVID());
-	LCD_ShowString(4, 22, ST7735Ctx.Width, 16, 16, text);
-	sprintf((char *)&text, "LCD ID:0x%lX", st7735_id);
-	LCD_ShowString(4, 40, ST7735Ctx.Width, 16, 16, text);
+    sprintf((char*)&text, "WeAct Studio");
+    LCD_ShowString(4, 4, ST7735Ctx.Width, 16, 16, text);
+    sprintf((char*)&text, "STM32H7xx 0x%lX", HAL_GetDEVID());
+    LCD_ShowString(4, 22, ST7735Ctx.Width, 16, 16, text);
+    sprintf((char*)&text, "LCD ID:0x%lX", st7735_id);
+    LCD_ShowString(4, 40, ST7735Ctx.Width, 16, 16, text);
 
-	LCD_Light(100, 200);
+    LCD_Light(100, 200);
 }
 
-void LCD_SetBrightness(uint32_t Brightness)
-{
-	__HAL_TIM_SetCompare(LCD_Brightness_timer, LCD_Brightness_channel, Brightness);
+void LCD_SetBrightness(uint32_t Brightness) {
+    __HAL_TIM_SetCompare(LCD_Brightness_timer, LCD_Brightness_channel,
+                         Brightness);
 }
 
-uint32_t LCD_GetBrightness(void)
-{
-		return __HAL_TIM_GetCompare(LCD_Brightness_timer, LCD_Brightness_channel);
+uint32_t LCD_GetBrightness(void) {
+    return __HAL_TIM_GetCompare(LCD_Brightness_timer, LCD_Brightness_channel);
 }
-
 
 // The screen gradually brightens or dims
 // Brightness_Dis: target brightness
 // time: time to reach the target brightness, in ms
-void LCD_Light(uint32_t Brightness_Dis,uint32_t time)
-{
-	uint32_t Brightness_Now;
-	uint32_t time_now;
-	float temp1,temp2;
-	float k,set;
+void LCD_Light(uint32_t Brightness_Dis, uint32_t time) {
+    uint32_t Brightness_Now;
+    uint32_t time_now;
+    float    temp1, temp2;
+    float    k, set;
 
-	Brightness_Now = LCD_GetBrightness();
-	time_now = 0;
-	if(Brightness_Now == Brightness_Dis)
-		return;
+    Brightness_Now = LCD_GetBrightness();
+    time_now       = 0;
+    if (Brightness_Now == Brightness_Dis)
+        return;
 
-	if(time == time_now)
-		return;
+    if (time == time_now)
+        return;
 
-	temp1 = Brightness_Now;
-	temp1 = temp1 - Brightness_Dis;
-	temp2 = time_now;
-	temp2 = temp2 - time;
+    temp1 = Brightness_Now;
+    temp1 = temp1 - Brightness_Dis;
+    temp2 = time_now;
+    temp2 = temp2 - time;
 
-	k = temp1 / temp2;
+    k = temp1 / temp2;
 
-	uint32_t tick=get_tick();
-	while(1)
-	{
-		delay_ms(1);
+    uint32_t tick = get_tick();
+    while (1) {
+        delay_ms(1);
 
-		time_now = get_tick()-tick;
+        time_now = get_tick() - tick;
 
-		temp2 = time_now - 0;
+        temp2 = time_now - 0;
 
-		set = temp2*k + Brightness_Now;
+        set = (temp2 * k) + Brightness_Now;
 
-		LCD_SetBrightness((uint32_t)set);
+        LCD_SetBrightness((uint32_t)set);
 
-		if(time_now >= time) break;
-
-	}
+        if (time_now >= time)
+            break;
+    }
 }
 
-uint16_t POINT_COLOR=0xFFFF;	// Brush color
-uint16_t BACK_COLOR=BLACK;  // Background color
+uint16_t POINT_COLOR = 0xFFFF; // Brush color
+uint16_t BACK_COLOR  = BLACK;  // Background color
 // Display a character at the specified coordinates
-//x,y: Starting coordinates
-//num: Characters to display: " "--->"~"
-//size: Font size: 12/16
-//mode: Overlay mode (1) or non-overlay mode (0)
-void LCD_ShowChar(uint16_t x,uint16_t y,uint8_t num,uint8_t size,uint8_t mode)
-{
-  uint8_t temp,t1,t;
-	uint16_t y0=y;
-	uint16_t x0=x;
-	uint16_t colortemp=POINT_COLOR;
-  uint32_t h,w;
+// x,y: Starting coordinates
+// num: Characters to display: " "--->"~"
+// size: Font size: 12/16
+// mode: Overlay mode (1) or non-overlay mode (0)
+void LCD_ShowChar(uint16_t x, uint16_t y, uint8_t num, uint8_t size,
+                  uint8_t mode) {
+    uint8_t  temp, t1, t;
+    uint16_t y0        = y;
+    uint16_t x0        = x;
+    uint16_t colortemp = POINT_COLOR;
+    uint32_t h, w;
 
-	uint16_t write[size][size==12?6:8];
-	uint16_t count;
+    uint16_t write[size][size == 12 ? 6 : 8];
+    uint16_t count;
 
-  ST7735_GetXSize(&st7735_pObj,&w);
-	ST7735_GetYSize(&st7735_pObj,&h);
+    ST7735_GetXSize(&st7735_pObj, &w);
+    ST7735_GetYSize(&st7735_pObj, &h);
 
-	//Set up the window
-	num=num-' ';//Get the offset value
-	count = 0;
+    // Set up the window
+    num   = num - ' '; // Get the offset value
+    count = 0;
 
-	if(!mode) // Non-overlapping mode
-	{
-		for(t=0;t<size;t++)
-		{
-			if(size==12)temp=asc2_1206[num][t];  // Use 1206 font
-			else temp=asc2_1608[num][t];		 // Use 1608 font
+    if (!mode) // Non-overlapping mode
+    {
+        for (t = 0; t < size; t++) {
+            if (size == 12)
+                temp = asc2_1206[num][t]; // Use 1206 font
+            else
+                temp = asc2_1608[num][t]; // Use 1608 font
 
-			for(t1=0;t1<8;t1++)
-			{
-				if(temp&0x80)
-					POINT_COLOR=(colortemp&0xFF)<<8|colortemp>>8;
-				else
-					POINT_COLOR=(BACK_COLOR&0xFF)<<8|BACK_COLOR>>8;
+            for (t1 = 0; t1 < 8; t1++) {
+                if (temp & 0x80)
+                    POINT_COLOR = (colortemp & 0xFF) << 8 | colortemp >> 8;
+                else
+                    POINT_COLOR = (BACK_COLOR & 0xFF) << 8 | BACK_COLOR >> 8;
 
-				write[count][t/2]=POINT_COLOR;
-				count ++;
-				if(count >= size) count =0;
+                write[count][t / 2] = POINT_COLOR;
+                count++;
+                if (count >= size)
+                    count = 0;
 
-				temp<<=1;
-				y++;
-				if(y>=h){POINT_COLOR=colortemp;return;}// Out of bounds
-				if((y-y0)==size)
-				{
-					y=y0;
-					x++;
-					if(x>=w){POINT_COLOR=colortemp;return;}// Out of bounds
-					break;
-				}
-			}
-		}
-	}
-	else// Overlay mode
-	{
-		for(t=0;t<size;t++)
-		{
-			if(size==12)temp=asc2_1206[num][t];  // Use 1206 font
-			else temp=asc2_1608[num][t];		 // Use 1608 font
-			for(t1=0;t1<8;t1++)
-			{
-				if(temp&0x80)
-					write[count][t/2]=(POINT_COLOR&0xFF)<<8|POINT_COLOR>>8;
-				count ++;
-				if(count >= size) count =0;
+                temp <<= 1;
+                y++;
+                if (y >= h) {
+                    POINT_COLOR = colortemp;
+                    return;
+                } // Out of bounds
+                if ((y - y0) == size) {
+                    y = y0;
+                    x++;
+                    if (x >= w) {
+                        POINT_COLOR = colortemp;
+                        return;
+                    } // Out of bounds
+                    break;
+                }
+            }
+        }
+    } else // Overlay mode
+    {
+        for (t = 0; t < size; t++) {
+            if (size == 12)
+                temp = asc2_1206[num][t]; // Use 1206 font
+            else
+                temp = asc2_1608[num][t]; // Use 1608 font
+            for (t1 = 0; t1 < 8; t1++) {
+                if (temp & 0x80)
+                    write[count][t / 2] =
+                        (POINT_COLOR & 0xFF) << 8 | POINT_COLOR >> 8;
+                count++;
+                if (count >= size)
+                    count = 0;
 
-				temp<<=1;
-				y++;
-				if(y>=h){POINT_COLOR=colortemp;return;} // Out of bounds
-				if((y-y0)==size)
-				{
-					y=y0;
-					x++;
-					if(x>=w){POINT_COLOR=colortemp;return;} // Out of bounds
-					break;
-				}
-			}
-		}
-	}
-	ST7735_FillRGBRect(&st7735_pObj,x0,y0,(uint8_t *)&write,size==12?6:8,size);
-	POINT_COLOR=colortemp;
+                temp <<= 1;
+                y++;
+                if (y >= h) {
+                    POINT_COLOR = colortemp;
+                    return;
+                } // Out of bounds
+                if ((y - y0) == size) {
+                    y = y0;
+                    x++;
+                    if (x >= w) {
+                        POINT_COLOR = colortemp;
+                        return;
+                    } // Out of bounds
+                    break;
+                }
+            }
+        }
+    }
+    ST7735_FillRGBRect(&st7735_pObj, x0, y0, (uint8_t*)&write,
+                       size == 12 ? 6 : 8, size);
+    POINT_COLOR = colortemp;
 }
 
 // Display a string at the specified coordinates
@@ -255,91 +264,93 @@ void LCD_ShowChar(uint16_t x,uint16_t y,uint8_t num,uint8_t size,uint8_t mode)
 // width,height: Window size
 // size: Font size: 12/16
 // *p: Starting address of the string
-void LCD_ShowString(uint16_t x,uint16_t y,uint16_t width,uint16_t height,uint8_t size,uint8_t *p)
-{
-	uint8_t x0=x;
-	width+=x;
-	height+=y;
-    while((*p<='~')&&(*p>=' '))// Check if the string is valid
+void LCD_ShowString(uint16_t x, uint16_t y, uint16_t width, uint16_t height,
+                    uint8_t size, uint8_t* p) {
+    uint8_t x0 = x;
+    width += x;
+    height += y;
+    while ((*p <= '~') && (*p >= ' ')) // Check if the string is valid
     {
-        if(x>=width){x=x0;y+=size;}
-        if(y>=height)break;
-        LCD_ShowChar(x,y,*p,size,0);
-        x+=size/2;
+        if (x >= width) {
+            x = x0;
+            y += size;
+        }
+        if (y >= height)
+            break;
+        LCD_ShowChar(x, y, *p, size, 0);
+        x += size / 2;
         p++;
     }
 }
 
-static int32_t lcd_init(void)
-{
-	int32_t result = ST7735_OK;
-	HAL_TIMEx_PWMN_Start(LCD_Brightness_timer,LCD_Brightness_channel);
-	return result;
+static int32_t lcd_init(void) {
+    int32_t result = ST7735_OK;
+    HAL_TIMEx_PWMN_Start(LCD_Brightness_timer, LCD_Brightness_channel);
+    return result;
 }
 
-static int32_t lcd_gettick(void)
-{
-	return HAL_GetTick();
+static int32_t lcd_gettick(void) {
+    return HAL_GetTick();
 }
 
-static int32_t lcd_writereg(uint8_t reg,uint8_t* pdata,uint32_t length)
-{
-	int32_t result;
-	LCD_CS_RESET;
-	LCD_RS_RESET;
-	result = HAL_SPI_Transmit(SPI_Drv,&reg,1,100);
-	LCD_RS_SET;
-	if(length > 0)
-		result += HAL_SPI_Transmit(SPI_Drv,pdata,length,500);
-	LCD_CS_SET;
-	if(result>0){
-		result = -1;}
-	else{
-		result = 0;}
-	return result;
+static int32_t lcd_writereg(uint8_t reg, uint8_t* pdata, uint32_t length) {
+    int32_t result;
+    LCD_CS_RESET;
+    LCD_RS_RESET;
+    result = HAL_SPI_Transmit(SPI_Drv, &reg, 1, 100);
+    LCD_RS_SET;
+    if (length > 0)
+        result += HAL_SPI_Transmit(SPI_Drv, pdata, length, 500);
+    LCD_CS_SET;
+    if (result > 0) {
+        result = -1;
+    } else {
+        result = 0;
+    }
+    return result;
 }
 
-static int32_t lcd_readreg(uint8_t reg,uint8_t* pdata)
-{
-	int32_t result;
-	LCD_CS_RESET;
-	LCD_RS_RESET;
+static int32_t lcd_readreg(uint8_t reg, uint8_t* pdata) {
+    int32_t result;
+    LCD_CS_RESET;
+    LCD_RS_RESET;
 
-	result = HAL_SPI_Transmit(SPI_Drv,&reg,1,100);
-	LCD_RS_SET;
-	result += HAL_SPI_Receive(SPI_Drv,pdata,1,500);
-	LCD_CS_SET;
-	if(result>0){
-		result = -1;}
-	else{
-		result = 0;}
-	return result;
+    result = HAL_SPI_Transmit(SPI_Drv, &reg, 1, 100);
+    LCD_RS_SET;
+    result += HAL_SPI_Receive(SPI_Drv, pdata, 1, 500);
+    LCD_CS_SET;
+    if (result > 0) {
+        result = -1;
+    } else {
+        result = 0;
+    }
+    return result;
 }
 
-static int32_t lcd_senddata(uint8_t* pdata,uint32_t length)
-{
-	int32_t result;
-	LCD_CS_RESET;
-	//LCD_RS_SET;
-	result =HAL_SPI_Transmit(SPI_Drv,pdata,length,100);
-	LCD_CS_SET;
-	if(result>0){
-		result = -1;}
-	else{
-		result = 0;}
-	return result;
+static int32_t lcd_senddata(uint8_t* pdata, uint32_t length) {
+    int32_t result;
+    LCD_CS_RESET;
+    // LCD_RS_SET;
+    result = HAL_SPI_Transmit(SPI_Drv, pdata, length, 100);
+    LCD_CS_SET;
+    if (result > 0) {
+        result = -1;
+    } else {
+        result = 0;
+    }
+    return result;
 }
 
-static int32_t lcd_recvdata(uint8_t* pdata,uint32_t length)
-{
-	int32_t result;
-	LCD_CS_RESET;
-	//LCD_RS_SET;
-	result = HAL_SPI_Receive(SPI_Drv,pdata,length,500);
-	LCD_CS_SET;
-	if(result>0){
-		result = -1;}
-	else{
-		result = 0;}
-	return result;
+static int32_t lcd_recvdata(uint8_t* pdata, uint32_t length) {
+    int32_t result;
+    LCD_CS_RESET;
+    // LCD_RS_SET;
+    result = HAL_SPI_Receive(SPI_Drv, pdata, length, 500);
+    LCD_CS_SET;
+    if (result > 0) {
+        result = -1;
+    } else {
+        result = 0;
+    }
+    return result;
 }
